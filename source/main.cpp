1ab81f38695d5ed9ba68524cf013cf264a1c3adb
Arden Rasmussen <ardenisthebest@gmail.com>
2017-09-10 23:02:00 -0700

    Initialized development

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,20 @@
-int main(int argc, char const *argv[]) {
+#include "tui.hpp"
+
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
   /* code */
+  printf("%i", tui::HasColor());
+  tui::Raw();
+  tui::NoEcho();
+  std::cout << "Enter a string: ";
+  std::string str;
+  std::cin >> str;
+  tui::Echo();
+  tui::NoCursor();
+  std::cout << "Enter a new string: ";
+  std::cin >> str;
+  tui::Cursor();
+  tui::NoRaw();
   return 0;
 }
